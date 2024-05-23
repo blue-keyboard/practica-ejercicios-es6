e5b8fe596fd743c60fbd0effb6a96819a60965fd
blue-keyboard <bluekeyboard9@gmail.com>
2024-05-23 13:03:09 +0200

    add exercise 4

--- ejercicio-03.js
+++ ejercicio-03.js
@@ -12,12 +12,19 @@ console.log(copyToy)
 // spread operatos.
 const pointsList2 = [32, 54, 21, 64, 75, 43]
 const pointsList3 = [54, 87, 99, 65, 32]
+const newPointList = [...pointsList2, ...pointsList3]
+console.log(newPointList)
 
 // 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
 // con spread operators.
 const toy2 = { name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor' }
 const toyUpdate = { lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk'] }
+const newToy = { ...toy2, ...toyUpdate }
+console.log(newToy)
 
 // 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
 // pero sin editar el array inicial. De nuevo, usando spread operatos.
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja']
+const index = 2
+const updatedColors = [...colors.slice(0, index), ...colors.slice(index + 1)]
+console.log(updatedColors)
